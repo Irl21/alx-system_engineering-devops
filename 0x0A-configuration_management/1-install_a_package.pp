3232c99fecdd1af10a5a7318a3f6feeb13fc2187
Irl21 <pwangai61@gmail.com>
2023-01-07 05:05:35 -0800

    fic

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,11 @@
-# install puppet-lint -v 2.5.0
+#!/usr/bin/pup
 
+# Install an especific version of flask (2.1.0)
 
+package {'flask':
 
-exec { 'puppet-lint':
+  ensure   => '2.1.0',
 
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+  provider => 'pip3'
 
 }
